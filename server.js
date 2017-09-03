1d0eea852ff76f28097dbf7fc335848eb48f4ce6
alexiswalker <alexiswalker@hotmail.com>
2017-09-03 19:50:17 +0000

    init

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-var express = require('express'),
-    app = express();
- 
+var http = require('http');
+var express = require('express');
 var app = express();
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
- 
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
-app.get('/', function(req, res) {
-    res.send('Hello from NodeJS  at '+ new Date());
+http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
- 
- 
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
 
-module.exports = app ;
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
